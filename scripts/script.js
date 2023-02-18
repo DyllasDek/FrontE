fdcceb224896624772c8b883ea19c234d9804ae0
DyllasDek <danchik.korneenko@yandex.ru>
2023-02-19 02:14:13 +0300

    Done js scripting

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,41 @@
 async function fetchComicID(email) {
     const params = new URLSearchParams();
     if (email) {
-        params.append('email', email);
+        params.append('email', email,true);
     }
+
+
     let xhr = new XMLHttpRequest(); 
     let url ='https://fwd.innopolis.app/api/hw2?' + params.toString()
-    xhr.open('GET', url)
-    xhr.send()
-    let responseObj;
-    xhr.onload = function() {
-        responseObj = xhr.responseText;
-        alert(responseObj); 
-    }
-    return responseObj;
+    let resp = await fetch(url, {
+        mode: 'cors',
+        headers: {
+          'Access-Control-Allow-Origin':'*'
+        }
+      }).then(r =>r.json());
+    return resp
 }
 
-async function requestSite(mail) {
-
-    let id = await fetchComicID(mail);    
-
-    json = await fetch("https://xkcd.com/"+id+"/info.0.json").then(r =>r.json());
-    console.log(result.month);
+async function requestSite(id) {
+    url = "https://cors-anywhere.herokuapp.com/https://xkcd.com/"+id+"/info.0.json"
+    let resp = await fetch(url, {
+        mode: 'cors',
+        headers: {
+          'Access-Control-Allow-Origin':'*'
+        }
+      }).then(r =>r.json());
+    console.log(resp)
+    let output = document.createElement("div")
+    let date = new Date(parseInt(resp.year),parseInt(resp.month)-1,parseInt(resp.day));
+    let dat = date.toLocaleDateString('ru-RU');
+  
+    
+    output.innerHTML = 
+    '<img src='+resp.img+' alt='+resp.alt+'>\n\
+     <h3>'+dat+'</h3>                       \n\
+    ';
+    
+    let out = document.getElementById("comic")
+    out.appendChild(output)
 }
-
-requestSite("d.korneenko@innopolis.university")
+fetchComicID("d.korneenko@innopolis.university").then(t => requestSite(t))
