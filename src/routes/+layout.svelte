06a1964ca919ab264d9e5f8832a52388b4b0eba9
DyllasDek <danchik.korneenko@yandex.ru>
2023-03-19 18:40:51 +0300

    f

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,10 @@
     import './styles.css';
     import SideNav from '$lib/sidenav.svelte';    
   </script>
-<main>
-    <div class="maincont">
-        <SideNav />
-    </div>
-    <div class="main maincont">
-    <slot />
-    </div>
-</main>
+
+<div class="maincont">
+    <SideNav />
+</div>
+<div class="main maincont">
+<slot />
+</div>
